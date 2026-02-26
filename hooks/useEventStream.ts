11166a2cb6bb6a28054989f7e2b936835b302fcd
Charlie Li <yl11475@nyu.edu>
2026-02-25 23:19:54 -0500

    Fix TypeScript type error in resetLights

--- hooks/useEventStream.ts
+++ hooks/useEventStream.ts
@@ -372,7 +372,7 @@ export function useEventStream() {
             1: 'on', 2: 'on', 3: 'on', 4: 'on',
             5: 'on', 6: 'on', 7: 'on', 8: 'on',
             9: 'on', 10: 'on', 11: 'on', 12: 'on',
-          },
+          } as Record<number, 'on' | 'off' | 'burst'>,
           lastUpdated: now,
         }
       };
